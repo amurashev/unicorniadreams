64f1968ca4d5c64212e4b8551186179f7de2b6d1
Andrey Murashev <andreymurashev@gmail.com>
2021-08-10 18:21:49 +0300

    Fix sitemap script

--- internals/sitemap.ts
+++ internals/sitemap.ts
@@ -1,26 +1,25 @@
 const { SitemapStream, streamToPromise } = require('sitemap')
 const { Readable } = require('stream')
 const fs = require('fs')
+require('dotenv').config()
 
 const LISTINGS = require('../data/listings.json')
 const CATEGORIES = require('../data/categories.json')
-const CONFIG = require('../next.config')
 
 const listings = Object.keys(LISTINGS).map((key) => LISTINGS[key].slug)
 const categories = Object.keys(CATEGORIES).map((key) => CATEGORIES[key].slug)
 
-const outputFile = './sitemap.xml'
+const outputFilePath = './public/sitemap.xml'
 
 let commonPages = [
   {
     url: '/',
-    // changefreq: "daily",
-    // priority: 0.3,
   },
   {
     url: '/contacts',
-    // changefreq: "daily",
-    // priority: 0.3,
+  },
+  {
+    url: '/collections',
   },
 ]
 
@@ -28,21 +27,17 @@ commonPages = commonPages
   .concat(
     categories.map((item) => ({
       url: `/${item}`,
-      // changefreq: "daily",
-      // priority: 0.3,
     }))
   )
   .concat(
     listings.map((item) => ({
       url: `/item/${item}`,
-      // changefreq: "daily",
-      // priority: 0.3,
     }))
   )
 
 // Create a stream to write to
 const stream = new SitemapStream({
-  hostname: CONFIG.HOSTNAME,
+  hostname: process.env.HOSTNAME,
 })
 
 // Return a promise that resolves with your XML string
@@ -50,18 +45,19 @@ try {
   streamToPromise(Readable.from(commonPages).pipe(stream))
     .then((data) => {
       const result = data.toString()
-      fs.writeFileSync(outputFile, result)
+      fs.writeFileSync(outputFilePath, result)
 
-      console.log(`${outputFile} is created`, commonPages)
+      console.log(`${outputFilePath} is created`)
+      console.log(commonPages)
       process.exit(0)
     })
     .catch((err) => {
-      console.log(`${outputFile} is not created: ${err}`)
+      console.log(`${outputFilePath} is not created: ${err}`)
       process.exit(1)
     })
 } catch (err) {
-  console.log(`${outputFile} is not created: ${err}`)
+  console.log(`${outputFilePath} is not created: ${err}`)
   process.exit(1)
 }
 
-export {}
+// export {}
--- next.config.js
+++ next.config.js
@@ -1,7 +1,3 @@
 module.exports = {
-  env: {
-    ETSY_API_KEY_STRING: "e3e8mrqeqrpz9zz4mjnopfb6",
-    ETSY_API_SECRET: "qdt957345c",
-  },
-  HOSTNAME: 'http://localhost:3000/'
+ 
 };
