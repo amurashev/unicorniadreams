6a8e47f9e3ed4700eb7dd5f498cdaf2517bdcf23
Andrey Murashev <andreymurashev@gmail.com>
2021-08-14 21:02:25 +0300

    Remove metrika

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -20,26 +20,6 @@ export default function App({ Component, pageProps }) {
     // gtag('js', new Date())
     // @ts-ignore
     // gtag('config', 'G-BH4EFRY18K')
-
-    ;(function (m, e, t, r, i, k, a) {
-      m[i] =
-        m[i] ||
-        function () {
-          ;(m[i].a = m[i].a || []).push(arguments)
-        }
-      m[i].l = 1 * new Date()
-      ;(k = e.createElement(t)),
-        (a = e.getElementsByTagName(t)[0]),
-        (k.async = 1),
-        (k.src = r),
-        a.parentNode.insertBefore(k, a)
-    })(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js', 'ym')
-
-    ym(84009604, 'init', {
-      clickmap: true,
-      trackLinks: true,
-      accurateTrackBounce: true,
-    })
   }, [])
 
   const props = {
