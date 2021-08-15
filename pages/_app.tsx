abd1322b72a8318ea240ec7eef6fce192c505457
Andrey Murashev <andreymurashev@gmail.com>
2021-08-15 15:14:36 +0300

    Remove mixpanel

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@
 import { useEffect } from 'react'
 import { useRouter } from 'next/router'
 import lozad from 'lozad'
-import mixpanel from 'mixpanel-browser'
+// import mixpanesl from 'mixpanel-browser'
 
 import CONFIG from '../data/config.json'
 
@@ -21,8 +21,8 @@ export default function App({ Component, pageProps }) {
     // gtag('js', new Date())
     // @ts-ignore
     // gtag('config', 'G-BH4EFRY18K')
-    mixpanel.init(CONFIG.mixPanel.token)
-    mixpanel.track('Site Open');
+    // mixpanel.init(CONFIG.mixPanel.token)
+    // mixpanel.track('Site Open');
   }, [])
 
   const props = {
