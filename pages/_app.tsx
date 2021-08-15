c0edcebf01e3a3b3c502e52342bfcd6567a57119
Andrey Murashev <andreymurashev@gmail.com>
2021-08-15 15:38:14 +0300

    Test gtag.js

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@
 import { useEffect } from 'react'
 import { useRouter } from 'next/router'
 import lozad from 'lozad'
-import ReactGA from 'react-ga';
 // import mixpanesl from 'mixpanel-browser'
 
 import CONFIG from '../data/config.json'
@@ -14,18 +13,16 @@ export default function App({ Component, pageProps }) {
   useEffect(() => {
     const observer = lozad() // lazy loads elements with default selector as '.lozad'
     observer.observe()
-    // ;(window as any).dataLayer = (window as any).dataLayer || []
-    // function gtag() {
-    //   ;(window as any).dataLayer.push(arguments)
-    // }
     // @ts-ignore
-    // gtag('js', new Date())
-    // @ts-ignore
-    // gtag('config', 'G-BH4EFRY18K')
+    window.dataLayer = (window as any).dataLayer || []
+    function gtag() {
+      window.dataLayer.push(arguments)
+    }
+    gtag('js', new Date())
+    gtag('config', CONFIG.googleAnalytics.key)
+
     // mixpanel.init(CONFIG.mixPanel.token)
     // mixpanel.track('Site Open');
-    ReactGA.initialize(CONFIG.googleAnalytics.key);
-    ReactGA.pageview(window.location.pathname + window.location.search);
   }, [])
 
   const props = {
