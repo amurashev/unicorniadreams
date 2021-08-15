80e4fda6e73d7ab3b731cbd993edbc7051f0b3b0
Andrey Murashev <andreymurashev@gmail.com>
2021-08-15 20:19:56 +0300

    Remove YM

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -22,25 +22,6 @@ export default function App({ Component, pageProps }) {
 
     // mixpanel.init(CONFIG.mixPanel.token)
     // mixpanel.track('Site Open');
-    ;(function (m, e, t, r, i, k, a) {
-      m[i] =
-        m[i] ||
-        function () {
-          ;(m[i].a = m[i].a || []).push(arguments)
-        }
-      m[i].l = 1 * new Date()
-      ;(k = e.createElement(t)),
-        (a = e.getElementsByTagName(t)[0]),
-        (k.async = 1),
-        (k.src = r),
-        a.parentNode.insertBefore(k, a)
-    })(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js', 'ym')
-
-    ym(84024415, 'init', {
-      clickmap: true,
-      trackLinks: true,
-      accurateTrackBounce: true,
-    })
   }, [])
 
   return <Component {...pageProps} />
